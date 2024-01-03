61bd1e008641ea56c144e7e3bd8815dfeeffdb17
Martin Emil Brabenec <martinemilbrabenec@gmail.com>
2024-01-03 12:57:10 +0100

    Initial commit with smart contract and project files

---
+++ contracts/RockPaperScissors.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract RockPaperScissors {
+    enum Move { Rock, Paper, Scissors }
+
+    function play(Move playerMove) public view returns (string memory) {
+        uint computerMove = uint(keccak256(abi.encodePacked(block.timestamp, block.difficulty))) % 3;
+        Move compMove = Move(computerMove);
+
+        if(playerMove == compMove) {
+            return "It's a draw!";
+        } else if ((playerMove == Move.Rock && compMove == Move.Scissors) || 
+                   (playerMove == Move.Scissors && compMove == Move.Paper) || 
+                   (playerMove == Move.Paper && compMove == Move.Rock)) {
+            return "Player wins!";
+        } else {
+            return "Computer wins!";
+        }
+    }
+}
